9f66f66d0ff99800862c25aba1009b70f2e6dd4e
Satish Verma <s4satishverma@gmail.com>
2026-06-19 08:31:09 -0400

    Update html2canvas.js

--- js/html2canvas.js
+++ js/html2canvas.js
@@ -1,9 +1,3 @@
-/*
-  html2canvas 0.5.0-alpha1 <http://html2canvas.hertzen.com>
-  Copyright (c) 2015 Niklas von Hertzen
-
-  Released under MIT License
-*/
 
 (function(window, document, exports, global, define, undefined){
 
